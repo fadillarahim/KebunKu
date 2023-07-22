cb7cba6a729768455b084c310772921a853f330e
fadillarahim <fadillarahimximipa1@gmail.com>
2023-07-22 21:22:52 +0700

    Chat done tapi belum di test errornya

--- app/src/main/java/com/tugas/kebunku/chat/Chat.kt
+++ app/src/main/java/com/tugas/kebunku/chat/Chat.kt
@@ -1,3 +1,17 @@
 package com.tugas.kebunku.chat
 
-data class Chat()
+import com.google.firebase.firestore.Exclude
+
+data class Chat(val username: String? = null,
+                val message: String? = null,
+                val time:String? = null){
+
+    @Exclude
+    fun getMap(): Map<String, Any?>{
+        return mapOf(
+            "username" to username,
+            "message" to message,
+            "time" to time
+        )
+    }
+}
