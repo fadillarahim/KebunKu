2006138ebf96288dcf24d41ad28aeb48f634f8bd
HanaOctavia <hoktavia03@gmail.com>
2023-07-22 17:44:23 +0700

    ProdukSold Activity

--- app/src/main/java/com/example/marketplacepertanian/productsold/ProdukSold.kt
+++ app/src/main/java/com/example/marketplacepertanian/productsold/ProdukSold.kt
@@ -3,6 +3,6 @@ package com.example.marketplacepertanian.productsold
 data class ProdukSold(
     var nama_produk: String? = null,
     var ukuran: String?= null,
-    var harga: String?= null,
+    var total_harga: String?= null,
     var quantity: String?= null,
 )
---
+++ app/src/main/java/com/example/marketplacepertanian/productsold/ProdukSoldAdapter.kt
@@ -0,0 +1,58 @@
+package com.example.marketplacepertanian.productsold
+
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.example.marketplacepertanian.R
+import com.example.marketplacepertanian.produk.EditProdukActivity
+import com.example.marketplacepertanian.produk.Produk
+import com.example.marketplacepertanian.produk.ProdukAdapter
+import com.google.firebase.storage.FirebaseStorage
+import java.io.File
+
+class ProdukSoldAdapter (private val produksoldList: ArrayList<ProdukSold>) :
+    RecyclerView.Adapter<ProdukSoldAdapter.ProdukSoldViewHolder>(){
+
+    private lateinit var activity: AppCompatActivity
+    class ProdukSoldViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val nama_produk: TextView = itemView.findViewById(R.id.TVLNamaProduk)
+        val ukuran_produk: TextView = itemView.findViewById(R.id.TVLUkuranProduk)
+        val total_harga: TextView = itemView.findViewById(R.id.TVLTotalHarga)
+        val img_produk: ImageView = itemView.findViewById(R.id.IMLFotoProduk)
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProdukSoldViewHolder {
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.produk_sold_list_layout, parent, false)
+        return ProdukSoldViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ProdukSoldViewHolder, position: Int) {
+        val produk_sold: ProdukSold = produksoldList[position]
+        holder.nama_produk.text = produk_sold.nama_produk
+        holder.ukuran_produk.text = produk_sold.ukuran
+        holder.total_harga.text = produk_sold.total_harga
+
+
+        val storageRef = FirebaseStorage.getInstance().reference.child("img_produk/${produk_sold.nama_produk}.jpg")
+        val localfile = File.createTempFile("tempImage", "jpg")
+        storageRef.getFile(localfile).addOnSuccessListener {
+            val bitmap = BitmapFactory.decodeFile(localfile.absolutePath)
+            holder.img_produk.setImageBitmap(bitmap)
+        }.addOnFailureListener{
+            Log.e("foto ?", "gagal")
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return  produksoldList.size
+    }
+}
