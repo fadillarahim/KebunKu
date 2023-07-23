420ded7d02ef3c5f3e822824bbeb1755067ecc8d
HanaOctavia <hoktavia03@gmail.com>
2023-07-23 21:20:28 +0700

    Activity Produk Sold Update

--- app/src/main/java/com/example/marketplacepertanian/productsold/ProdukSoldAdapter.kt
+++ app/src/main/java/com/example/marketplacepertanian/productsold/ProdukSoldAdapter.kt
@@ -43,16 +43,6 @@ class ProdukSoldAdapter (private val produksoldList: ArrayList<ProdukSold>) :
         holder.total_harga.text = produk_sold.total_harga
         holder.kuantitas_produk.text = produk_sold.quantity
 
-        holder.itemView.setOnClickListener {
-            activity = it.context as AppCompatActivity
-            activity.startActivity(Intent(activity, EditProductSoldActivity::class.java).apply {
-                putExtra("nama", produk_sold.nama_produk.toString())
-                putExtra("berat", produk_sold.berat.toString())
-                putExtra("total_harga", produk_sold.total_harga.toString())
-                putExtra("quantity", produk_sold.quantity.toString())
-
-            })
-        }
 
         val storageRef = FirebaseStorage.getInstance().reference.child("img_produk/${produk_sold.nama_produk}.jpg")
         val localfile = File.createTempFile("tempImage", "jpg")
